fdf4e7b9bb3e6d184869813f936f4ff843edc67d
Liyao Chen <gliyao@gmail.com>
2015-05-22 21:29:38 +0800

    Initial commit

---
+++ AdoptTaipei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdoptTaipei
+//
+//  Created by Liyao on 2015/5/22.
+//  Copyright (c) 2015年 Liyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdoptTaipei/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdoptTaipei
+//
+//  Created by Liyao on 2015/5/22.
+//  Copyright (c) 2015年 Liyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
